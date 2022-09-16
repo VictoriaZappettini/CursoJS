bc61ce940a7e16ee3b944fd82e3b68ed0167a367
Victoria <55468124+VictoriaZappettini@users.noreply.github.com>
2022-09-15 21:56:30 -0300

    Agrego clase alumno + metodo join()

--- js/promedio.js
+++ js/promedio.js
@@ -1,38 +1,46 @@
+function Alumno (nombre, apellido, notas) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.notas = notas;
+
+}
 let notas=[];
+let resultado=0; 
 
 function recibirNotas (){
     let total=0;
-    let resultado=0; 
+    
 
 for(i = 0;i < cantidad;i++){
 notas[i]=parseFloat(prompt("Ingresa la primer nota"+' '+[i+1]));
 total= total+ notas[i]
 }
 
-resultado= total/cantidad;
-
 document.write("<br>");
 document.write("Las notas ingresadas fueron:");
 document.write("<br>");
 document.write(notas.join(' '));
-//notas.sort();
-/*for(let i = 0;i < cantidad ;i++){
-    document.write(notas[i]);
-    document.write("<br>");
-} */
+
 
 
 document.write("<br>");
 document.write ("Total:"+''+total)
 document.write("<br>");
-document.write ("El promedio final es:"+''+resultado)
+//document.write ("El promedio final es:"+''+resultado)
+
+return resultado= total/cantidad;
 
 
 }
 
 
+
+let nombre = prompt ("Nombre del alumno");
+let apellido= prompt ("Apellido del alumno");
 let cantidad = parseInt( prompt("En cuantas notas se divide la nota final? "));
 recibirNotas();
-
-
-
+let datosAlumno = new Alumno (nombre,apellido,notas)
+document.write("<br>");
+document.write ("El alumno se llama:"+''+nombre+apellido)
+document.write("<br>");
+document.write ("El promedio final es:"+''+resultado)
