2195262534a13f5f47ab30ca3a973d31b1b2371b
Victoria <55468124+VictoriaZappettini@users.noreply.github.com>
2022-09-20 20:12:22 -0300

    Agrego comunicación con DOM

--- js/promedio.js
+++ js/promedio.js
@@ -1,35 +1,35 @@
-function Alumno (nombre, apellido, notas) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.notas = notas;
-
+class Alumno {
+    constructor (nombre, apellido, notas){
+        this.nombre= nombre;
+        this.apellido = apellido;
+        this.notas = notas;
+    }
 }
+
+//Variables: 
 let notas=[];
 let resultado=0; 
 
 function recibirNotas (){
     let total=0;
-    
-
-for(i = 0;i < cantidad;i++){
-notas[i]=parseFloat(prompt("Ingresa la primer nota"+' '+[i+1]));
-total= total+ notas[i]
-}
-
-document.write("<br>");
-document.write("Las notas ingresadas fueron:");
-document.write("<br>");
-document.write(notas.join(' '));
-
-
-
-document.write("<br>");
-document.write ("Total:"+''+total)
-document.write("<br>");
-//document.write ("El promedio final es:"+''+resultado)
-
-return resultado= total/cantidad;
-
+    for(i = 0;i < cantidad;i++){
+        notas[i]=parseFloat(prompt("Ingresa la primer nota"+' '+[i+1]));
+        total= total+ notas[i]
+        }
+        
+        document.write("<br>");
+        document.write("Las notas ingresadas fueron:");
+        document.write("<br>");
+        document.write(notas.join(' '));
+        
+        
+        
+        //document.write("<br>");
+        //document.write ("Total:"+''+total)
+        //document.write("<br>");
+        //document.write ("El promedio final es:"+''+resultado)
+        
+        return resultado= total/cantidad;
 
 }
 
@@ -40,7 +40,14 @@ let apellido= prompt ("Apellido del alumno");
 let cantidad = parseInt( prompt("En cuantas notas se divide la nota final? "));
 recibirNotas();
 let datosAlumno = new Alumno (nombre,apellido,notas)
+
 document.write("<br>");
-document.write ("El alumno se llama:"+''+nombre+apellido)
-document.write("<br>");
-document.write ("El promedio final es:"+''+resultado)
+const tituloPrincipal = document.getElementById('tituloPrincipal');
+tituloPrincipal.innerHTML = `<p>Alumno: ${nombre} ${apellido}</p>`
+
+const promedio = document.getElementById('promedio');
+promedio.innerHTML = `<p>El promedio final es : ${resultado} </p>`
+
+
+
+
